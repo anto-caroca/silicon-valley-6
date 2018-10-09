d3f38a466ea2ee45fa6b716eb487604e81d8291d
antonella <antonella_caroca@hotmail.com>
2018-10-08 21:11:13 -0300

    cambio api de pixabay por api de unsplash y se renderizan fotos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
-import Buttons from './Buttons';
-import Pin  from './Pin';
+import Navbar from './Components/Navbar';
+import Buttons from './Components/Buttons';
+import ImgList from './Components/ImgList';
+
+
 import './App.css';
 
 
 
 class App extends Component {
+
+  constructor() {
+		super();
+		this.state = {
+			imgs: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api.unsplash.com/photos/?client_id=6d941d1ebb2255c24c2b6903e6bb9e25049f5ae6ffbe08b8c8d8eeeb063336b2')
+			.then(res => res.json())
+			.then(data => {
+				this.setState({ imgs: data });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	}
+
   render() {
     return (
       <div className="App">
         <Navbar/>
        <Buttons/>
-       <Pin />
+       <div className="main-content">
+					<ImgList data={this.state.imgs} />
+				</div>
        
       </div>
     );
--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,6 +1,6 @@
-import './App.css';
+import '../App.css';
 import React, { Component } from 'react';
-import './App';
+import '../App';
 
  class Buttons extends Component {
     render(){
