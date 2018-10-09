8ec2b7e4dd71f9194b99f87a310e333055bb9b4c
antonella <antonella_caroca@hotmail.com>
2018-10-09 17:22:29 -0300

    Updates

---
+++ precache-manifest.cf576d5745291c7565ae1e8a5dbc09bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0911cc2507a61abbcbb",
+    "url": "/static/js/main.c0911cc2.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "c0911cc2507a61abbcbb",
+    "url": "/static/css/main.a1868473.chunk.css"
+  },
+  {
+    "revision": "47805607ca63c8c02863c83a759f7161",
+    "url": "/index.html"
+  }
+];
